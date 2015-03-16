27ff9375f1484646693f8ce08dfd13598b46da49
horuscentro <horus.central3@gmail.com>
2015-03-15 21:54:38 -0600

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -2,23 +2,23 @@ echo ' Instalaremos "GIT" para clonar los repositorios de los programas necesari
 echo '--------------------------------------------------------------------------------'
 sudo apt-get install git
 echo 'Clonamos los repos'
-echo 'Drm-intel->'
+echo 'Drm-intel...'
 git clone git://people.freedesktop.org/~danvet/drm-intel
-echo ' Drm->'
+echo ' Drm...'
 git clone git://anongit.freedesktop.org/git/mesa/drm
-echo '2d driver->'
+echo '2d driver...'
 git clone git://anongit.freedesktop.org/git/xorg/driver/xf86-video-intel
-echo 'Mesa->'
+echo 'Mesa...'
 git clone git://anongit.freedesktop.org/git/mesa/mesa
-echo 'Libva->'
+echo 'Libva...'
 git clone git://anongit.freedesktop.org/vaapi/libva
-echo 'Driver Intel->'
+echo 'Driver Intel...'
 git clone git://anongit.freedesktop.org/vaapi/intel-driver
-echo 'Cairo->'
+echo 'Cairo...'
 git clone git://anongit.freedesktop.org/cairo
-echo 'Intel GPU Tools->'
+echo 'Intel GPU Tools...'
 git clone git://anongit.freedesktop.org/git/xorg/app/intel-gpu-tools
-echo 'Fuentes del kernel 3.18' 
+echo 'Fuentes del kernel 3.18 ...' 
 wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.18.tar.xz
 echo 'Descomprimiendo kernel...'
 tar -xf linux-3.18.tar.xz
