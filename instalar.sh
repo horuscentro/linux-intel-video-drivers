218a11a9af3408539d6cec696500a2f9aca26661
horuscentro <horus.central3@gmail.com>
2015-03-15 23:02:33 -0600

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,26 +1,41 @@
+echo '      3rd Generation Intel® Core™ Processors with Intel® HD Graphics 4000.'
 echo ' Instalaremos "GIT" para clonar los repositorios de los programas necesarios.'
 echo '--------------------------------------------------------------------------------'
 sudo apt-get install git
 echo 'Clonamos los repos'
-echo 'Drm-intel...'
-git clone git://people.freedesktop.org/~danvet/drm-intel
-echo ' Drm...'
-git clone git://anongit.freedesktop.org/git/mesa/drm
-echo '2d driver...'
-git clone git://anongit.freedesktop.org/git/xorg/driver/xf86-video-intel
-echo 'Mesa...'
-git clone git://anongit.freedesktop.org/git/mesa/mesa
-echo 'Libva...'
-git clone git://anongit.freedesktop.org/vaapi/libva
-echo 'Driver Intel...'
-git clone git://anongit.freedesktop.org/vaapi/intel-driver
-echo 'Cairo...'
-git clone git://anongit.freedesktop.org/cairo
-echo 'Intel GPU Tools...'
-git clone git://anongit.freedesktop.org/git/xorg/app/intel-gpu-tools
+echo 'libdrm-2.4.58.tar.gz...'
+wget http://dri.freedesktop.org/libdrm/libdrm-2.4.58.tar.gz
+tar -xvzf libdrm-2.4.58.tar.gz
+echo '/////'
+echo 'xf86-video-intel-2.99.917.tar.gz...'
+wget http://xorg.freedesktop.org/archive/individual/driver/xf86-video-intel-2.99.917.tar.gz
+tar -xvzf xf86-video-intel-2.99.917.tar.gz
+echo '/////'
+echo 'Mesa 10.04...'
+wget ftp://ftp.freedesktop.org/pub/mesa/10.4.0/MesaLib-10.4.0.tar.bz2
+tar -xvzf MesaLib-10.4.0.tar.bz2
+echo '/////'
+echo 'libva-1.5.0.tar.bz2...'
+wget http://www.freedesktop.org/software/vaapi/releases/libva/libva-1.5.0.tar.bz2
+tar -xvzf libva-1.5.0.tar.bz2
+echo '/////'
+echo 'libva-intel-driver-1.5.0.tar.bz2...'
+wget http://www.freedesktop.org/software/vaapi/releases/libva-intel-driver/libva-intel-driver-1.5.0.tar.bz2
+tar -xvzf libva-intel-driver-1.5.0.tar.bz2 
+echo '/////'
+echo 'cairo-1.12.16.tar.xz...'
+wget  http://cairographics.org/releases/cairo-1.12.16.tar.xz
+tar -xvzf cairo-1.12.16.tar.xz
+echo '/////'
+echo 'intel-gpu-tools-1.9.tar.bz2...'
+wget http://xorg.freedesktop.org/archive/individual/app/intel-gpu-tools-1.9.tar.bz2
+tar -xvzf intel-gpu-tools-1.9.tar.bz2
+echo '/////'
 echo 'Fuentes del kernel 3.18 ...' 
 wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.18.tar.xz
+echo '/////'
 echo 'Descomprimiendo kernel...'
 tar -xf linux-3.18.tar.xz
+echo '/////'
 echo 'kernel descpmŕimdo->'
 echo '--------------------------------------------------------------------------------'
